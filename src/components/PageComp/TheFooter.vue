724f6bce5126ca0507d17c118106a8910aa8c5e3
Asthriona <im@asthriona.com>
2022-04-21 22:25:59 +0200

    v1.0

---
+++ src/components/PageComp/TheFooter.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="Footer text-center">
+      <v-footer v-bind="localAttrs" :padless="padless" dark>
+          <v-card-text class="white--text">
+            2019 - {{ new Date().getFullYear() }} —
+            <strong class="text-uppercase">Moon</strong> <span class="text-uppercase">guild</span> - All Right Reserved.<br />
+            Code Made by
+            <a
+              href="https://rainboomstudio.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Rainboom Studio</a
+            > ●
+            Hosting
+            <a
+              href="https://team.yukiko.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Yukiko Dev Team</a
+            > ●
+            DNS Powered by
+            <a
+              href="https://Asthriona.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Asthriona ltd.</a
+            ><br />
+            Domain uptime: {{ longTime }} <br />
+            <div class="foot_logo">
+              <p>Operating Company</p>
+              <div class="figure">
+                <img
+                  src="https://cdn.nishikino.xyz/asthriona/icons/footLogo.png"
+                  alt="Asthriona Logo"
+                  style="width: 100%"
+                />
+              </div>
+            </div>
+          </v-card-text>
+      </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'The-Footer',
+    data() {
+        return {
+            icons: [
+                'mdi-home',
+                'mdi-email',
+                'mdi-calendar',
+                'mdi-account',
+                'mdi-delete',
+            ],
+            longTime: 0,
+            padless: true,
+            variant: 'absolute',
+        }
+    },
+      methods: {
+    //Code by Aimee (http://www.mangoya.cn/#/)
+    runTime() {
+      //运行倒计时
+      var oldTime = new Date("2022/04/21 22:09:00");
+      setInterval(() => {
+        const nowTime = new Date();
+        const longTime = nowTime - oldTime;
+        const days = parseInt(longTime / 1000 / 60 / 60 / 24, 10); //计算剩余的天数
+        const hours = parseInt((longTime / 1000 / 60 / 60) % 24, 10); //计算剩余的小时
+        const minutes = parseInt((longTime / 1000 / 60) % 60, 10); //计算剩余的分钟
+        const seconds = parseInt((longTime / 1000) % 60, 10); //计算剩余的秒数
+        this.longTime =
+          days +
+          " Days, " +
+          hours +
+          " Hours, " +
+          minutes +
+          " Minutes, " +
+          seconds +
+          " Seconds, ";
+      }, 1000);
+      console.log(
+        "Moon Guild\nHosted by Nishikino Networks, \nDNS by Asthriona ltd.\nCode by Asthriona \nTheme by Asthriona"
+      );
+    }
+  },
+  created() {
+    this.runTime();
+  },
+    computed: {
+        localAttrs() {
+            const attrs = {}
+        if(this.variant === 'default') {
+            attrs.absolute = false,
+            attrs.fixed = false
+        } else {
+            attrs[this.variant] = true
+        }
+        return attrs
+        },
+    }
+};
+</script>
+
+<style scoped>
+.foot_logo {
+  display: table;
+  max-width: 300px;
+  width: 100%;
+  margin: 0 auto 12px;
+}
+.foot_logo p {
+  display: table-cell;
+  width: 9em;
+  vertical-align: middle;
+  font-size: 12px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <v-banner>
+        <div class="header">
+          <span class="font-weight-light display-3">We Are</span> <br />
+          <p><span class="font-weight-bold text-uppercase display-4">Moon</span>
+          <span class="font-weight-light text-uppercase display-4">Guild</span></p>
+          <span class="font-weight-light headline">Your new World Of Warcraft Experiance</span>
+        </div>
+      </v-banner>
+      <v-row>
+        <v-col cols="12">
+          <div class="article">
+          <p>
+            <span class="display-2">Who are We?</span> <br>
+            <span class="font-weight-bold text-uppercase">Moon</span>
+          <span class="font-weight-light text-uppercase">Guild</span>
+          is a recently created guild, based on Magtheridon we put focus around socials, and progressions. Our goal is first of all having fun all together playing this amazing game that is World Of Warcraft. We are also a group of people who are all passionate about the game and want to make it better.
+          <b>We do not try hard!</b> try hard might be good for some, but we know from experiance whene to stop to keep the experiance fun for everyone, No Try Hard at Moon Guild, Just fun!
+          If you too want to experiance the Moon Guild, join us!
+          </p>
+          <p>
+            <span class="display-1"> Events</span> <br>
+            Here we love those! WoW events havent been made to be experianced alone! Lots of us like to take part in it, sitting in voice chat (Muted or not we wont judge), and playing together.
+          </p>
+          <p>
+            <span class="display-1">Acceptation</span> <br>
+            Everyone is welcome in Moon Guild! No one is here to judge anyone, and our staff allways keep an eye out to make sure eveyone is accepted. <br>
+            We have player from every horizons, They, and you are all the most welcome!
+          </p>
+          </div>
+        </v-col>
+        <v-col cols="12" class="text-center">
+          <span class="display-2">Join us!</span> <br>
+          <div class="icon" v-for="icon in icons" :key="icon.name">
+            <a :href="icon.link" class="no-link" target="_blank" rel="noopener noreferrer"> <v-icon size="50">{{ icon.icon }}</v-icon></a>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      icons: [
+        {
+          name: "Discord",
+          icon: "mdi-discord",
+          link: "https://discord.gg/Vq6wC3dyNh"
+        },
+        {
+          name: "WoW Armory",
+          icon: "mdi-armory",
+          link: "https://worldofwarcraft.com/en-gb/guild/eu/magtheridon/moon-guild"
+        }
+      ]
+    }
+  },
+};
 </script>
+
+<style scoped>
+.header {
+  text-align: center;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.no-link {
+  color: #fff !important;
+  text-decoration: none !important;
+  cursor: pointer !important;
+}
+</style>
